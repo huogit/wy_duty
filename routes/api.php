d4b36ef3e881901d0f4871edbafba732a8f9475e
zhenli <2650316229@qq.com>
2019-08-25 00:11:02 +0800

    ...

--- routes/api.php
+++ routes/api.php
@@ -37,7 +37,7 @@ Route::group(['middleware' => 'CheckToken'], function() {
     Route::post('/duty/apply/auditing', 'DutyController@auditing')->middleware('CheckAdmin');   /* 审批操作 */
 
     // 通讯录模块
-    Route::get('/address_book', 'AddressBookController@list');                         /* 通讯录列表 */
+    Route::get('/address_book/list', 'AddressBookController@list');                         /* 通讯录列表 */
     Route::get('/address_book/search', 'AddressBookController@search');                /* 搜索联系人 */
     Route::get('/address_book/{id}', 'AddressBookController@addressBook');             /* 联系人详细信息 */
 
